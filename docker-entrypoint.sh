5ba260ab911fd0c90e085fe201ac584207469c40
fuzoh <info@bastiennicoud.ch>
2026-01-23 14:33:30 +0100

    build: max requests for frankenPHP, prevents memory leaks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,5 @@ php artisan migrate --force
 echo "Optimizing cache..."
 php artisan optimize
 
-php artisan octane:start --server=frankenphp
+# Restart worker every 1000 requests to prevent memory leaks
+php artisan octane:start --server=frankenphp --max-requests=1000
