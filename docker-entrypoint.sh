a32144647905f39559ff53d38ac9b06b9be05e3a
fuzoh <info@bastiennicoud.ch>
2026-01-23 14:42:13 +0100

    build: frankenPHP start port and host

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "Starting application..."
 
-echo "🔄 Running migrations..."
+echo "Running migrations..."
 php artisan migrate --force
 
 echo "Optimizing cache..."
 php artisan optimize
 
 # Restart worker every 1000 requests to prevent memory leaks
-php artisan octane:start --server=frankenphp --max-requests=1000
+php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=8000 --max-requests=1000
