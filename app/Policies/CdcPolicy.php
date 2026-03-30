594b587ecb6694d8b3e7a03104ec398e6658b5ff
Bilal Janir <bilaljanir@gmail.com>
2026-03-30 18:28:15 +0200

    refactore:add super admin override in FormPolicy and CdcPolicy

--- app/Policies/CdcPolicy.php
+++ app/Policies/CdcPolicy.php
@@ -2,11 +2,16 @@
 
 namespace App\Policies;
 
+use App\Helpers\RoleHelper;
 use App\Models\Cdc;
 use App\Models\User;
 
 class CdcPolicy
 {
+    public function before(User $user, string $ability): ?bool
+    {
+        return $user->hasRole(RoleHelper::ROLE_SUPER_ADMIN) ? true : null;
+    }
     public function view(User $user, Cdc $cdc): bool
     {
         return $cdc->form->user_id === $user->id;
--- app/Policies/FormPolicy.php
+++ app/Policies/FormPolicy.php
@@ -2,11 +2,17 @@
 
 namespace App\Policies;
 
+use App\Helpers\RoleHelper;
 use App\Models\Form;
 use App\Models\User;
 
 class FormPolicy
 {
+    public function before(User $user, string $ability): ?bool
+    {
+        return $user->hasRole(RoleHelper::ROLE_SUPER_ADMIN) ? true : null;
+    }
+
     public function view(User $user, Form $form): bool
     {
         return $user->id === $form->user_id;
